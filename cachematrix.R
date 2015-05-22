213237ee1c7a11ffb18906380ba00deb1b5839b0
Ismael Gomez <gomez.ismael@gmail.com>
2015-05-23 00:11:53 +0200

    Assignement 2. Caching inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+          ## Create "matrix" object (list of functions) from matrix
+
+    # Initialization for first time the "matrix" is created
+    Inv = NULL
+
+    # Setter for when we reassign the original matrix to the "matrix"
+    set = function(Y) {
+        # new matrix to consider
+        x <<- Y
+        # reinitialization for the inverted matrix cache
+        Inv <<- NULL
+    }
+
+    # give back the matrix
+    get = function() x
+
+    # set the cached value (will be used after calculation)
+    setInv = function(InvMat) Inv <<- InvMat
+
+    # get the cached value
+    getInv = function() Inv
+
+    # return "matrix" which is a list of functions w/ closure
+    list(set = set,
+         get = get,
+         setInv = setInv,
+         getInv = getInv)
 
 }
 
@@ -12,4 +39,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    # get the current cached value
+    Inv = x$getInv()
+
+    # check the cached value
+    if(!is.null(Inv)){
+        # return the cache value, as it is meaningfull
+        message("getting cached data")
+        return(Inv)
+    }
+
+    # otherwise (cached value not yet calculated)
+        # get matrix from closure of the "matrix" functions
+    data = x$get()
+        # calculate inverted matrix
+    Inv = solve(data, ...)
+        # set it as the cached value in the "matrix"
+    x$setInv(Inv)
+        # return the calculated (and cached) value
+    Inv
 }
